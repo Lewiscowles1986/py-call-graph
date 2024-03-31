9ac3af0500fd759aaefbb01f006ef932423b1e76
Lewis Cowles <lewis+github@cowles.uk>
2024-03-31 15:44:57 +0100

    release: 2.1.1

--- pycallgraph/metadata.py
+++ pycallgraph/metadata.py
@@ -1,6 +1,6 @@
 # A different file to pycallgraph.py because of circular import problem
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 __copyright__ = 'Copyright Gerald Kaszuba 2007-2024'
 __license__ = 'GPLv2'
 __author__ = 'Gerald Kaszuba'
