89f1eea080b910f73301d16f3e374b158941aaaa
Lewis Cowles <lewis+github@cowles.uk>
2025-03-22 16:37:04 +0000

    ci: try matrix with 3.13.2 (#34)
    * ci: try matrix with 3.13.2
    
    * ci: update test, curated test and lint to use 3.13.2
    
    * ci: use yaml anchors to DRY python version matrix definition
    
    * ci: pivot from yaml anchors to a job and using outputs with dependencies
    
    * ci: compact jq for version matrix script
    
    * chore: update package metadata setup.py to include python 3.13
    
    * docs: manpage and metadata for new release
    
    * ci: min and max versions per python release
    
    * ci: exclude 3.8 from min max due to pipeline failures

--- pycallgraph/metadata.py
+++ pycallgraph/metadata.py
@@ -1,7 +1,7 @@
 # A different file to pycallgraph.py because of circular import problem
 
-__version__ = '2.1.2'
-__copyright__ = 'Copyright Gerald Kaszuba 2007-2024'
+__version__ = '2.1.3'
+__copyright__ = 'Copyright Gerald Kaszuba 2007-2025'
 __license__ = 'GPLv2'
 __author__ = 'Gerald Kaszuba'
 __email__ = 'pycallgraph@gakman.com'
